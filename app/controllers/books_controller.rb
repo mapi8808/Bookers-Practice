c6330d49385892aa981b80ca05708d48526c9894
mapi8808 <mapi.008.k@gmail.com>
2020-12-29 14:09:57 +0000

    [add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,6 +13,12 @@ class BooksController < ApplicationController
   def edit
   end
   
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path
+  end
+  
   private
   def books_params
     params.require(:book).permit(:title, :body)
